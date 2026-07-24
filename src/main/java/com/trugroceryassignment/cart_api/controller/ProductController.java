787e7ed5173ba6449dc1703512fe6f5ff6c41357
sachan13harshit <harshit.23bcs10134@sst.scaler.com>
2026-07-25 05:23:55 +0530

    removed unused import

--- src/main/java/com/trugroceryassignment/cart_api/controller/ProductController.java
+++ src/main/java/com/trugroceryassignment/cart_api/controller/ProductController.java
@@ -5,14 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.trugroceryassignment.cart_api.dto.response.ProductResponse;
 import com.trugroceryassignment.cart_api.service.ProductService;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequiredArgsConstructor
